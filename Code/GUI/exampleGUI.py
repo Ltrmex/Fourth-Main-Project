60b8e6bea8242db5fc11ee055d073937f6dadc65
Ltrmex <maciekmajchrzak23@wp.pl>
2019-02-06 23:19:22 +0000

    Picture widget

--- Code/GUI/exampleGUI.py
+++ Code/GUI/exampleGUI.py
@@ -1,11 +1,12 @@
 # Imports
-from guizero import App, Text, TextBox, PushButton, Slider
+from guizero import App, Text, TextBox, PushButton, Slider, Picture 
 
 # Method responsible for changing the value of welcomeMessage
 def displayMyName():
     welcomeMessage.value = myName.value
     return
 
+# Method responsible for changing text size value
 def changeTextSize(sliderValue):
     welcomeMessage.size = sliderValue
     return
@@ -25,6 +26,9 @@ updateText = PushButton(app, command=displayMyName, text="Display my name")
 
 # Slider widget
 textSize = Slider(app, command=changeTextSize, start=10, end=80)
+
+# Picture widget
+image = Picture(app, image="C:/Users/Ltrmex/Desktop/Fourth-Main-Project/Code/GUI/Images/Python.png")
 
 # Display app window
 app.display()   
