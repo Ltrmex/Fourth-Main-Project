2ccb543afbfd4d71a84ca655fb11f5bfd2450605
Ltrmex <maciekmajchrzak23@wp.pl>
2019-02-06 23:10:20 +0000

    text and button widgets

--- Code/GUI/exampleGUI.py
+++ Code/GUI/exampleGUI.py
@@ -1,11 +1,23 @@
 # Imports
-from guizero import App, Text
+from guizero import App, Text, TextBox, PushButton 
+
+# Method responsible for changing the value of welcomeMessage
+def displayMyName():
+    welcomeMessage.value = myName.value
+    return
 
 # Set app name
 app = App(title="Hello world")
 
 # Widgets (text, text boxes, buttons, etc)
+# Text widget
 welcomeMessage = Text(app, text="Welcome to my app", size=40, font="Times New Roman", color="lightblue")
+
+# Text box widget
+myName = TextBox(app)
+
+# Update text button
+updateText = PushButton(app, command=displayMyName, text="Display my name")
 
 # Display app window
 app.display()   
