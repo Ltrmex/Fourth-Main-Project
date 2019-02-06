a867515b9b121ead9d72b01f94fa596e4eff000e
Ltrmex <maciekmajchrzak23@wp.pl>
2019-02-06 23:02:36 +0000

    text widget

--- Code/GUI/exampleGUI.py
+++ Code/GUI/exampleGUI.py
@@ -1,4 +1,11 @@
-from guizero import App
+# Imports
+from guizero import App, Text
 
+# Set app name
 app = App(title="Hello world")
-app.display()
+
+# Widgets (text, text boxes, buttons, etc)
+welcomeMessage = Text(app, text="Welcome to my app", size=40, font="Times New Roman", color="lightblue")
+
+# Display app window
+app.display()   
