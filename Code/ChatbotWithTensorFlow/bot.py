8518137829aa1082a463ea5968670553a6615dcb
kamilam1987 <kamilam1987@gmail.com>
2019-02-21 16:49:48 +0000

    opens testing file

--- Code/ChatbotWithTensorFlow/bot.py
+++ Code/ChatbotWithTensorFlow/bot.py
@@ -18,8 +18,50 @@ def create_table():
     c.execute("""CREATE TABLE IF NOT EXIST parent_reply(parent_id TEXT PRIMARY KEY,
      comment_id TEXT UNIQUE, parent TEXT, comment TEXT, subreddit TEXT, unix INT, score INT)""")
 
+def format_data(data):
+    # Repleace a new line, return and ""
+    data = data.repleace("\n", "newlinechar").repleace("\r", "newlinechar").repleace('"', "'")
+    return data
+# Find parent by parent id
+def find_parent(pid):
+    try:
+        sql = "SELECT comment FROM parent_reply WHERE comment_id = '{}' LIMIT 1".format(pid)
+        c.execute(sql)
+        result = c.fetchone()
+        if result != None:
+            # Only selecting from comment
+            return result[0]
+        else:
+            return False
+    # Error exception
+    except Exception as e:
+        #print("find_parent",e) 
+        return False
+    
+    
 if __name__ == "__main__":
     create_table()
+    # Rows in a file
+    row_counter = 0
+    # Parent and child pairs
+    paired_rows = 0
+
+    # Opens a file
+    with open("C:/Users/Kamilka/Year4/Traning_Data/{}/RC_{}".format(timeframe.split('-')[0], timeframe), buffer = 1000) as f:
+        # Iterate through f
+        for row in f:
+            row_counter += 1
+            row = json.loads(row)
+            parent_id = row['parent_id']
+            body = format_data(row['body'])
+            created_utc = row['created_utc']
+            score = row['score']
+            subreddit = row['subreddit']
+
+            parent_data = find_parent(parent_id)
+
+
+    
 
 
 
