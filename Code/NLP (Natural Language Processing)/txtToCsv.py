ca80df3a79bf411b4c3fe56ec173c9e7223beff6
Ltrmex <maciekmajchrzak23@wp.pl>
2019-03-04 03:31:41 +0000

    data generation

--- Code/NLP (Natural Language Processing)/txtToCsv.py
+++ Code/NLP (Natural Language Processing)/txtToCsv.py
@@ -1,37 +1,44 @@
 import csv
 from nltk import sent_tokenize
+def corpusDialogueData():
+    
 
-# load text
-filename = 'C:/Users/Ltrmex/Desktop/Fourth-Main-Project/Code/NLP (Natural Language Processing)/sample.txt'
-file = open(filename, encoding="utf8")
-text = file.readlines()
-file.close()
+def wikipediaData():
+    # load text
+    filename = 'C:/Users/Ltrmex/Desktop/Fourth-Main-Project/Code/NLP (Natural Language Processing)/sample.txt'
+    file = open(filename, encoding="utf8")
+    text = file.readlines()
+    file.close()
 
-# define lists
-article = []
-question = []
-answer = []
+    # define lists
+    article = []
+    question = []
+    answer = []
 
-i=0
-while i < len(text):
-    sentences = sent_tokenize(text[i])
+    i=0
+    while i < len(text):
+        sentences = sent_tokenize(text[i])
 
-    if i != 0:
-        article.append(sentences[0])
-        question.append(sentences[1])
-        answer.append(sentences[2])
+        if i != 0:
+            article.append(sentences[0])
+            question.append(sentences[1])
+            answer.append(sentences[2])
 
-    i += 1
-
-with open('output.csv', 'w') as f:
-    w = csv.writer(f, delimiter=',')
-    w.writerow(['Article', 'Question', 'Answer'])
-
-    i = 0
-    while i < len(article):
-        w.writerow([article[i], question[i], answer[i],])
         i += 1
-     
-f.close()
 
-print "CSV saved!"
+    with open('output.csv', 'w') as f:
+        w = csv.writer(f, delimiter=',')
+        w.writerow(['Article', 'Question', 'Answer'])
+
+        i = 0
+        while i < len(article):
+            w.writerow([article[i], question[i], answer[i],])
+            i += 1
+        
+    f.close()
+
+    print "CSV saved!"
+
+
+# wikipediaData()
+# corpusDialogueData()
